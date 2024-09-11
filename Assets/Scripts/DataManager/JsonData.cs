2ec9503485a66388514a833a1a23218280f20642
ywang <ywang22@mails.tsinghua.edu.cn>
2024-09-11 16:31:12 +0800

    feat: change backend info class and convert backend info to jsondata

--- Assets/Scripts/DataManager/JsonData.cs
+++ Assets/Scripts/DataManager/JsonData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 
 namespace DataManager
@@ -37,6 +38,12 @@ namespace DataManager
         public List<Block> NewBlocks { get; set; }
         public List<Block> EliminateBlocks { get; set; }
 
+        public StateChange(List<Block> newBlocks, List<Block> eliminateBlocks)
+        {
+            NewBlocks = newBlocks;
+            EliminateBlocks = eliminateBlocks;
+        }
+
         public StateChange(List<List<int>> newBlocks, List<List<int>> eliminateBlocks)
         {
             NewBlocks = new List<Block>();
@@ -55,7 +62,7 @@ namespace DataManager
     
     public class JsonData
     {
-        // 回放文件每一行的格式，同时也是后端发过来的格式
+        // 回放文件每一行的格式
         public int Round { get; set; }
         public int Player { get; set; }
         public int Steps { get; set; }
@@ -69,6 +76,71 @@ namespace DataManager
             Steps = -1;
             Operation = null;
             StateChanges = null;
+        }
+    }
+
+    public class BackendData
+    {
+        public int Round { get; set; }
+        public int Player { get; set; }
+        public int Steps { get; set; }
+        public Operation Operation { get; set; }
+        public List<List<Block>> ManyTimesNewBlocks { get; set; }
+        public List<List<Block>> ManyTimesEliminateBlocks { get; set; }
+
+        public BackendData()
+        {
+            Round = -1;
+            Player = -1;
+            Steps = -1;
+            Operation = null;
+            ManyTimesNewBlocks = null;
+            ManyTimesEliminateBlocks = null;
+        }
+
+        public BackendData(int round, int player, int steps, Operation operation, List<List<List<int>>> newBlocks,
+            List<List<List<int>>> eliminateBlocks)
+        {
+            Round = round;
+            Player = player;
+            Steps = steps;
+            Operation = operation;
+            ManyTimesNewBlocks = new List<List<Block>>();
+            ManyTimesEliminateBlocks = new List<List<Block>>();
+            foreach (var oneTimeNewBlocks in newBlocks)
+            {
+                var thisTime = new List<Block>();
+                foreach (var newBlock in oneTimeNewBlocks)
+                {
+                    thisTime.Add(new Block(newBlock));
+                }
+                ManyTimesNewBlocks.Add(thisTime);
+            }
+
+            foreach (var oneTimeEliminateBlocks in eliminateBlocks)
+            {
+                var thisTime = new List<Block>();
+                foreach (var eliminateBlock in oneTimeEliminateBlocks)
+                {
+                    thisTime.Add(new Block(eliminateBlock));
+                }
+                ManyTimesEliminateBlocks.Add(thisTime);
+            }
+        }
+        
+        // 将后端传过来的信息转换为前端可以解析的JsonData
+        public static JsonData Convert(BackendData backendData)
+        {
+            if (backendData.ManyTimesNewBlocks.Count != backendData.ManyTimesEliminateBlocks.Count)
+            {
+                return null;
+            }
+            var jsonData = new JsonData{Round = backendData.Round, Player = backendData.Player, Steps = backendData.Steps, Operation = backendData.Operation};
+            var stateChanges = new List<StateChange>();
+            var manyTimesBlockChanges = backendData.ManyTimesNewBlocks.Zip(backendData.ManyTimesEliminateBlocks, (n, e) => new StateChange(n, e));
+            stateChanges.AddRange(manyTimesBlockChanges);
+            jsonData.StateChanges = stateChanges;
+            return jsonData;
         }
     }
 
