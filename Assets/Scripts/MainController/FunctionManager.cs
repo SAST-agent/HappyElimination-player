cb62d116d33e6c8e1ba13f582467657255177113
ywang <ywang22@mails.tsinghua.edu.cn>
2024-09-17 00:19:43 +0800

    feat: give handle message API

--- Assets/Scripts/MainController/FunctionManager.cs
+++ Assets/Scripts/MainController/FunctionManager.cs
@@ -31,58 +31,44 @@ public class FunctionManager: MonoBehaviour
     }
     
     private static Queue<Tuple<Delegate, Arg>> _functionQueue;
-    private static bool _isProcessing;
 
     private void Start()
     {
         _functionQueue = new Queue<Tuple<Delegate, Arg>>();
-        _isProcessing = false;
-    }
-    
-    private void Update()
-    {
-        // TODO 增加一个检测动画是否播完的标志量，在func里面修改这个标志量，这里只负责读
-        if (!_isProcessing && _functionQueue.Count != 0)
-        {
-            Debug.Log($"bbb{_functionQueue.Count}");
-            var tuple = _functionQueue.Dequeue();
-            var func = tuple.Item1;
-            var arg = tuple.Item2;
-            LockAcquire();
-            if (arg.Index > 0)
-            {
-                func.DynamicInvoke(arg.Index);
-            }
-            else if (arg.Data.Round > 0)
-            {
-                func.DynamicInvoke(arg.Data);
-            }
-            else
-            {
-                func.DynamicInvoke();
-            }
-            LockRelease();
-        }
     }
 
     public void AddMessage(Delegate function, Arg arg)
     {
-        Debug.Log("AddMessage");
         if (_functionQueue == null)
         {
             _functionQueue = new Queue<Tuple<Delegate, Arg>>();
         }
         _functionQueue.Enqueue(new Tuple<Delegate, Arg>(function, arg));
+        Debug.Log("AddMessage");
         Debug.Log($"aaa{_functionQueue.Count}");
     }
 
-    private void LockRelease()
+    public void HandleMessage()
     {
-        _isProcessing = false;
-    }
-
-    private void LockAcquire()
-    {
-        _isProcessing = true;
+        Debug.Log("HandleMessage");
+        if (_functionQueue == null || _functionQueue.Count == 0)
+        {
+            return;
+        }
+        var tuple = _functionQueue.Dequeue();
+        var func = tuple.Item1;
+        var arg = tuple.Item2;
+        if (arg.Index > 0)
+        {
+            func.DynamicInvoke(arg.Index);
+        }
+        else if (arg.Data.Round > 0)
+        {
+            func.DynamicInvoke(arg.Data);
+        }
+        else
+        {
+            func.DynamicInvoke();
+        }
     }
 }
